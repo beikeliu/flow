8627bca25ebe94f98abf06bfa71231b9408a78f2
v_beileiliu <v_beileiliu@tencent.com>
2023-05-08 14:32:52 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import config from './config.json'
+import { useVueFlow, VueFlow } from '@vue-flow/core'
+
+useVueFlow(config)
+</script>
+
+<template>
+  <VueFlow />
+</template>
+
+<style scoped></style>
